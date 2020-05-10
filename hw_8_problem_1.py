9489321a9ca3c54a4324fcfaefdc7f8a5c6a0cba
soundreaper <soundreaper@gmail.com>
2020-05-09 21:14:06 -0700

    complete first problem

--- hw_8_problem_1.py
+++ hw_8_problem_1.py
@@ -1,17 +1,18 @@
 """
-Given a string s, find the longest palindromic substring in s. You may assume that the maximum length of s is 1000.
+Given a roman numeral, convert it to an integer. Input is guaranteed to be within the range from 1 to 3999.
 """
 
-def longestPalindrome(s):
-    m = ''
-    for i in range(len(s)):
-        for j in range(len(s), i, -1):
-            if len(m) >= j-i:
-                break
-            elif s[i:j] == s[i:j][::-1]:
-                m = s[i:j]
-                break
-    return m
+def romanToInt(s):
+    roman = {'M': 1000,'D': 500 ,'C': 100,'L': 50,'X': 10,'V': 5,'I': 1} # Dictionary creation O(1) time
+    z = 0
+    for i in range(0, len(s) - 1): # Loop through the input, could be up to "n" characters long so O(n)
+        if roman[s[i]] < roman[s[i+1]]: # Accessing a dictionary is O(1) time
+            z -= roman[s[i]] # Accessing a dictionary is O(1) time
+        else:
+            z += roman[s[i]] # Accessing a dictionary is O(1) time
+    return z + roman[s[-1]] # Accessing a dictionary is O(1) time
 
-input = "babad"
-print(longestPalindrome(input))
+input = "XIV"
+print(romanToInt(input))
+
+# Overall the time complexity looks to be O(n) time.
